0e27dd67ab1819005726e6cdb1603e98ad76d205
erum <erum.hannan@arpatech.com>
2016-06-20 14:44:27 +0500

    added message

--- app/src/main/java/com/sample/fcmimplementation/MainActivity.java
+++ app/src/main/java/com/sample/fcmimplementation/MainActivity.java
@@ -9,7 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //no need to write any code here  to get token.On application install it will check for internet permission if internet connects it will automatically
+        //fetch DeviceToken.
 
     }
 }
